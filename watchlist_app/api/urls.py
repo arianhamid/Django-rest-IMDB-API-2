4796b93a070e0be6d127ee4f01b149cf852ea598
Hamid <hamidarianyegane@gmail.com>
2023-10-11 01:08:52 +0330

    review-detail added using mixins

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from watchlist_app.api.views import WatchListAV, WatchDetailAV, StreamPlatformAV, StreamPlatformDetailAV
+from watchlist_app.api.views import WatchListAV, WatchDetailAV, StreamPlatformAV, StreamPlatformDetailAV,ReviewList, ReviewDetail
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie_list'),
     path('list/<int:pk>', WatchDetailAV.as_view(), name='movie_details'),
     path('stream/', StreamPlatformAV.as_view(), name='stream'),
     path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='stream_details'),
+    path('review/', ReviewList.as_view(), name='review_list'),
+    path('review/<int:pk>', ReviewDetail.as_view(), name='review_details'),
 ]
