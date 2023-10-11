3b6c8cfa7add9974e81dcc959f30a76b42457dc8
Hamid <hamidarianyegane@gmail.com>
2023-10-11 13:18:48 +0330

    Rewriting watchlist API using class-based views

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -4,26 +4,22 @@ from rest_framework.response import Response
 from rest_framework import mixins
 from rest_framework import generics
 from django.http import Http404
-from watchlist_app.api.serializers import WatchListSerializer, StreamPlatformSerializer,ReviewSerializer
+from watchlist_app.api.serializers import WatchListSerializer, StreamPlatformSerializer, ReviewSerializer
 from watchlist_app.models import WatchList, StreamPlatform, Review
 
 
-class ReviewDetail(mixins.RetrieveModelMixin,
-                 mixins.UpdateModelMixin,
-                 mixins.DestroyModelMixin,
-                 generics.GenericAPIView):
-    queryset = Review.objects.all()
-    serializer_class = ReviewSerializer
+# Using generic class-based views
+class WatchListAV(generics.ListCreateAPIView):
+    queryset = WatchList.objects.all()
+    serializer_class = WatchListSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-    
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-    
+class WatchDetailAV(generics.RetrieveUpdateDestroyAPIView):
+    queryset = WatchList.objects.all()
+    serializer_class = WatchListSerializer
+
+
+# Using mixins
 class ReviewList(mixins.ListModelMixin,
                  mixins.CreateModelMixin,
                  generics.GenericAPIView):
@@ -37,6 +33,24 @@ class ReviewList(mixins.ListModelMixin,
         return self.create(request, *args, **kwargs)
 
 
+class ReviewDetail(mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.DestroyModelMixin,
+                   generics.GenericAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+# using class-based views
 class StreamPlatformAV(APIView):
     def get(self, request, format=None):
         platform = StreamPlatform.objects.all()
@@ -51,51 +65,6 @@ class StreamPlatformAV(APIView):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
-
-
-class WatchListAV(APIView):
-    def get(self, request):
-        movies = WatchList.objects.all()
-        serializer = WatchListSerializer(
-            movies, many=True, context={'request': request})
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = WatchListSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
-
-
-class WatchDetailAV(APIView):
-    def get_object(self, pk):
-        try:
-            return WatchList.objects.get(pk=pk)
-        except WatchList.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        movie = self.get_object(pk=pk)
-        serializer = serializer = WatchListSerializer(
-            movie, context={'request': request})
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        movie = self.get_object(pk=pk)
-        serializer = WatchListSerializer(
-            movie, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        movie = self.get_object(pk=pk)
-        movie.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class StreamPlatformDetailAV(APIView):
